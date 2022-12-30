d41f128f3cb6c3477371de5494a37bb245212c7d
vikash <vikash0cv@gmail.com>
2022-12-30 13:41:19 +0530

    added header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
- import './App.css';
+import './App.css';
+import Header from './Components/header';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hi</h1>
+      <Header/>
     </div>
   );
 }
---
+++ src/Components/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './header.css';
+
+const Header = () => {
+  return (
+    <header className="beige-header">
+      <h1>Welcome to my website!</h1>
+    </header>
+  );
+}
+
+export default Header;
